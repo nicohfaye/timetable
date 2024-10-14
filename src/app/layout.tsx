da837536ee54f036edca3ecf7fd28d085eb5b759
Nicolai Faye <nicohfaye@gmail.com>
2024-10-14 18:32:26 +0200

    Initial commit, set up project skeleton.

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Timetable",
+  description: "My personal timetable display",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
